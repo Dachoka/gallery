00131d7fca55bfea16977bea4410610f9fb5d31f
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-12 10:24:20 +0300

    add a class for images

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    
+
