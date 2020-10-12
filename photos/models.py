7e908eebf2b32abbece032748f64156c3ad4a81c
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-12 10:31:50 +0300

    add a class for categories

--- photos/models.py
+++ photos/models.py
@@ -15,3 +15,11 @@ class Location(models.Model):
         return self.location
 
 
+class Category(models.Model):
+    category = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.category
+
+
+
