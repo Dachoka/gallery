76ce9b01d39e9649330bffae8b7de4229189b568
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-12 11:06:11 +0300

    add test to check the set up of location instance

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+
+class LocationTestCase(TestCase):
+
+    def setUp(self):
+        self.nairobi = Location(location = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi, Location))
