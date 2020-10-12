3af9956d7fcfe0c40a9619ca3176818255143b41
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-12 22:37:29 +0300

    add a view function that searches anad returns images given a category

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
+from .models import Image, Category,Location
 
 # Create your views here.
 def homepage(request):
     '''
     view function that renders the homepage
     '''
-    return HttpResponse('Welcome to Photo Blast')
+    return render(request,'homepage.html')
+
+def search_category(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_category = request.GET.get("category")
+        searched_categories = Image.search_by_category(search_category)
+        message = f"{search_category}"
+
+        return render(request, 'categorysearch.html', {"message":message, "images":searched_categories})
+
+    else:
+        message = "You have not searched for any category"
+        return render(request, 'categorysearch.html',{"message":message})
+
+        
+
+
